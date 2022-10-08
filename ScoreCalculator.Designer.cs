f88aab579731ea23b65f9dec781b7e16fc5b3f45
Naveen Reddy <nrkb1998@gmail.com>
2022-10-08 03:53:06 -0400

    Added finished score calculatore

---
+++ ScoreCalculator.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ScoreCalculator
+{
+    partial class ScoreCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtScoreTotal = new System.Windows.Forms.TextBox();
+            this.txtScoreCount = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClearScores = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(249, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score total:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(239, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Score count :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(261, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Average:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(349, 68);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(74, 27);
+            this.txtScore.TabIndex = 1;
+            // 
+            // txtScoreTotal
+            // 
+            this.txtScoreTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScoreTotal.Location = new System.Drawing.Point(349, 116);
+            this.txtScoreTotal.Name = "txtScoreTotal";
+            this.txtScoreTotal.ReadOnly = true;
+            this.txtScoreTotal.Size = new System.Drawing.Size(74, 27);
+            this.txtScoreTotal.TabIndex = 4;
+            this.txtScoreTotal.TabStop = false;
+            // 
+            // txtScoreCount
+            // 
+            this.txtScoreCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScoreCount.Location = new System.Drawing.Point(349, 170);
+            this.txtScoreCount.Name = "txtScoreCount";
+            this.txtScoreCount.ReadOnly = true;
+            this.txtScoreCount.Size = new System.Drawing.Size(74, 27);
+            this.txtScoreCount.TabIndex = 6;
+            this.txtScoreCount.TabStop = false;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAverage.Location = new System.Drawing.Point(349, 219);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(74, 27);
+            this.txtAverage.TabIndex = 7;
+            this.txtAverage.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(476, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(113, 32);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(476, 282);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(113, 32);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClearScores
+            // 
+            this.btnClearScores.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnClearScores.FlatAppearance.BorderSize = 2;
+            this.btnClearScores.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnClearScores.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearScores.Location = new System.Drawing.Point(310, 282);
+            this.btnClearScores.Name = "btnClearScores";
+            this.btnClearScores.Size = new System.Drawing.Size(113, 32);
+            this.btnClearScores.TabIndex = 3;
+            this.btnClearScores.Text = "&Clear scores";
+            this.btnClearScores.UseVisualStyleBackColor = true;
+            this.btnClearScores.Click += new System.EventHandler(this.btnClearScores_Click);
+            // 
+            // ScoreCalculator
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClearScores);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtScoreCount);
+            this.Controls.Add(this.txtScoreTotal);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "ScoreCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ScoreCalculator";
+         //   this.Load += new System.EventHandler(this.ScoreCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtScoreTotal;
+        private System.Windows.Forms.TextBox txtScoreCount;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClearScores;
+    }
+}
+
---
+++ ScoreCalculator.cs
@@ -0,0 +1,96 @@
+﻿/**************************************************************************************************************************************
+ * Naveen Reddy
+ * C0838989
+ * 2022-02-11
+ * TEST - 1
+ * Question - 2
+***************************************************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScoreCalculator
+{
+    public partial class ScoreCalculator : Form
+    {
+        // global variables to use in the functions:
+        int scoretotal;
+        int scorecount;
+        public ScoreCalculator()
+        {
+            InitializeComponent();
+        }
+        /*****************************************************************************************************************************
+         * this method loads the after click event on btnExit
+         * and this method is used to close the application
+         ****************************************************************************************************************************/
+        private void btnExit_Click(object sender, EventArgs e)
+        {  // this will close the window or the apllcication
+            this.Close();
+        }
+
+
+        /*****************************************************************************************************************************
+         * this method loads the after click event on btnAdd
+         * this methods add the user score and calculates the count, avg.
+        ******************************************************************************************************************************/
+        private void btnAdd_Click(object sender, EventArgs e)
+        {   // variables to store average and score.
+            decimal average;
+            // here we take the score input from user.
+            int score = Convert.ToInt32(txtScore.Text);
+            // scoretotal is calculated by adding score to it.
+            scoretotal += score;
+            // scorecount is calculated by adding 1 to its every click.
+            scorecount += 1;
+            // calculating average .
+            average = scoretotal / scorecount ;
+
+            // printing the calculated total score.
+            txtScoreTotal.Text = scoretotal.ToString();
+            // printing the calculated total print.
+            txtScoreCount.Text = scorecount.ToString();
+            // printing the average .
+            txtAverage.Text = average.ToString();
+            // cursor waits to the user at score text box and shows focus.
+            txtScore.Focus();
+            // clears the score text box and makes empty text box.
+            txtScore.Text = "";
+        }
+
+        /****************************************************************************************************************************
+         * this method loads the after click event on btnExit
+         * this method will clear all the previous data in the text boxes.
+         ****************************************************************************************************************************/
+        private void btnClearScores_Click(object sender, EventArgs e)
+        {   // here we are making scoretotal and scorecount values to zero 
+            // after clicking the btnclearscore it shoud clear all the values 
+            scoretotal = 0;
+            scorecount = 0;
+
+            // here we are making textaverage,score,count and txttotal to empty text box
+            txtAverage.Text = "";
+            txtScore.Text = "";
+            txtScoreCount.Text = "";
+            txtScoreTotal.Text = "";
+            // cursor waits to the user at score text box and shows focus.
+            txtScore.Focus();
+            
+        }
+    }
+}
+/**************************************************************************************************************************************
+ *                                                              JOKE
+ **************************************************************************************************************************************              
+ * Customer : "waiter, do you serve crabs?"
+ * Waiter : "Please sit down sir, we serve everyone."
+ **************************************************************************************************************************************
+ *                                                         END OF THE TEST - 1
+ **************************************************************************************************************************************
+ */
